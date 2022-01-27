0621839da5af6f447f50dc7255115910e5dc96a1
Ahmed Alatawi <aalat001@gmail.com>
2022-01-27 18:32:28 -0500

    Created with Expo CLI

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
+import 'react-native-gesture-handler';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistCache } from 'apollo3-cache-persist';
+import AppLoading from 'expo-app-loading';
+
+import HomeScreen from './components/HomeScreen';
+import ChapterScreen from './components/ChapterScreen';
+import { screenOptions } from './components/styles';
+
+const Stack = createNativeStackNavigator();
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  uri: 'https://api.graphql.guide/graphql',
+  cache,
+  defaultOptions: { watchQuery: { fetchPolicy: 'cache-and-network' } },
+});
 
 export default function App() {
+  const [loadingCache, setLoadingCache] = useState(true);
+
+  useEffect(() => {
+    persistCache({
+      cache,
+      storage: AsyncStorage,
+    }).then(() => setLoadingCache(false));
+  }, []);
+
+  if (loadingCache) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: '📖 The GraphQL Guide' }}
+          />
+          <Stack.Screen
+            name="Chapter"
+            component={ChapterScreen}
+            options={({
+              route: {
+                params: {
+                  chapter: { number, title },
+                },
+              },
+            }) => ({
+              title: number ? `Chapter ${number}: ${title}` : title,
+              gestureResponseDistance: { horizontal: 500 },
+            })}
+          />
+        </Stack.Navigator>
+        <StatusBar style="light" />
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
